c034c7ca78c364a17d76a2b510a474bb553c2ab8
Mohd Rejoan <wearecrx@gmail.com>
2022-03-27 23:50:30 +0600

    portal backdrop complete

--- src/component/RespLeftNavBar.jsx
+++ src/component/RespLeftNavBar.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import usePortals from '../HOC/usePortals'
 import LeftNavbar from './LeftNavbar'
 
-const RespLeftNavBar = () => {
+const RespLeftNavBar = (props) => {
   return (
-    <div className=' w-9/12 max-w-[320px] bg-white h-full'>
+    <div className=' w-9/12 max-w-[320px] bg-white h-full relative z-50'>
       <LeftNavbar />
     </div>
   )
